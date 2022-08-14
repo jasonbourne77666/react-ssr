95718dea680968c7a6bd03c01889e400ed02b95c
jasonbourne77666 <jasonbourne77666@gmail.com>
2022-08-14 21:01:21 +0800

    feat: test

--- src/shared/pages/login/index.tsx
+++ src/shared/pages/login/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import styles from './index.module.scss';
 
 function Login() {
-  return <div className={'test'}>Login</div>;
+  return (
+    <div className={'test'}>
+      <h3>Login</h3>
+      <Link to='/'>首页</Link>
+    </div>
+  );
 }
 
 export default Login;
