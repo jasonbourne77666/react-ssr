d61cba6ee4dfa5fa9ee13d8886d944c0be4fa8bf
jasonbourne77666 <jasonbourne77666@gmail.com>
2022-08-14 21:10:57 +0800

    feat: test

--- src/shared/pages/home/data.ts
+++ src/shared/pages/home/data.ts
@@ -1,10 +1,10 @@
 export const list = [
   {
-    title: '头像1',
+    title: '网络头像1',
     img: 'https://img1.baidu.com/it/u=4216761644,15569246&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=500',
   },
   {
-    title: '头像2',
+    title: '网络头像2',
     img: 'https://img0.baidu.com/it/u=2106001853,3367829453&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
   },
 ];
